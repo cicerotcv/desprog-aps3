1e11738a320fd8361f149deb2c98a241c796d575
cicerotcv <elm.tiago@gmail.com>
2021-04-12 13:09:06 -0300

    unit :ok:

--- dna.c
+++ dna.c
@@ -16,30 +16,33 @@ void load_string(FILE *file, int *p, char *s, int t) {
 }
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-    int possible_result;
-
-    if (m == 0 || n == 0) {
+    // casos particulares
+    if (n == 0 || m == 0) {
         length[n][m] = 0;
-    }
-    possible_result = length[m][n];
-    // caso trivial
-    if (possible_result != -1) {
-        return length[m][n];
+        return length[n][m];  // return evita processamento desnecessário
     }
 
-    return mlcs(a, m, b, n);
+    if (a[n - 1] == b[m - 1]) {
+        length[n][m] = 1 + mlcs_w(a, n - 1, b, m - 1, length);
+        return length[n][m];
+    }
+
+    int l1 = mlcs_w(a, n - 1, b, m, length);
+    int l2 = mlcs_w(a, n, b, m - 1, length);
+
+    return l1 > l2 ? l1 : l2;
 }
 
 int mlcs(char a[], int n, char b[], int m) {
-    if (n > 0 && m > 0) {
-        if (a[n - 1] == b[m - 1]) {
-            return mlcs(a, n - 1, b, m - 1) + 1;
+    int length[MAX_SIZE + 1][MAX_SIZE + 1];
+
+    for (int row_index = 0; row_index <= n; row_index++) {
+        for (int column_index = 0; column_index <= m; column_index++) {
+            length[row_index][column_index] = -1;
         }
-        int first = mlcs(a, n - 1, b, m);
-        int second = mlcs(a, n, b, m - 1);
-        return first > second ? first : second;
     }
-    return 0;
+
+    return mlcs_w(a, n, b, m, length);
 }
 
 int dlcs(char a[], int n, char b[], int m) { return 2; }
